e07f6e5c9b774e762040c0c6d9f2cb7aff405b68
SrArmas <ricardoarmas77@icloud.com>
2021-09-30 22:34:30 +0200

    changes2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Title content="Some Simple Title" />
+        <Pokemon />
+        <Pokemon
+          name="Charizard"
+          weight={90}
+          awesome={true}
+          terrifying={false}
+          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
+        />
+        <Pokemon
+          name="Bulbasaur"
+          weight={16.9}
+          awesome={true}
+          terrifying={false}
+          abilities={["Overgrow", "Chlorophyll"]}
+        />
+        <Pokemon
+          name="Mewtwo"
+          weight={122}
+          awesome={true}
+          terrifying={true}
+          abilities={["Pressure", "Unnerve", "Steadfast", "Insomnia"]}
+        />
+        <Pokemon
+          name="Mega beedrill"
+          weight={65}
+          awesome={false}
+          terrifying={true}
+          abilities={["Intimidate", "Unnerve"]}
+        />
+      </main>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Jaimita Te quiero!</code> 
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
